dbc706332cc4697b725782a43dfcbdf98d4916f8
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-11-14 17:21:09 +0300

    More Components (Extending Post Page)

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,13 +1,25 @@
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostData, PostProps } from "@/interfaces";
+import { useState } from "react";
 
 interface PostsPageProps {
     posts: PostProps[];
 }
 
 const Posts: React.FC<PostsPageProps> = ({ posts }) => {
-    console.log(posts);
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [allPosts, setAllPosts] = useState<PostProps[]>(posts);
+
+    const handleAddPost = (newPost: PostData) => {
+        const postWithId: PostProps = {
+            ...newPost,
+            id: allPosts.length + 1
+        };
+        setAllPosts(prevPosts => [postWithId, ...prevPosts]);
+    };
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
@@ -15,12 +27,15 @@ const Posts: React.FC<PostsPageProps> = ({ posts }) => {
                 <div className="container mx-auto">
                     <div className="flex justify-between items-center mb-6">
                         <h1 className="text-2xl font-semibold">Post Content</h1>
-                        <button className="bg-blue-700 px-4 py-2 rounded-full text-white hover:bg-blue-800 transition">
+                        <button 
+                            onClick={() => setModalOpen(true)}
+                            className="bg-blue-700 px-4 py-2 rounded-full text-white hover:bg-blue-800 transition"
+                        >
                             Add Post
                         </button>
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        {posts?.map((post: PostProps) => (
+                        {allPosts?.map((post: PostProps) => (
                             <PostCard 
                                 key={post.id}
                                 title={post.title} 
@@ -32,18 +47,24 @@ const Posts: React.FC<PostsPageProps> = ({ posts }) => {
                     </div>
                 </div>
             </main>
+
+            {isModalOpen && (
+                <PostModal 
+                    onClose={() => setModalOpen(false)} 
+                    onSubmit={handleAddPost} 
+                />
+            )}
         </div>
     );
 };
 
 export async function getStaticProps() {
-    // This line must contain the JSONPlaceholder URL
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const posts = await response.json();
-    
+
     return {
         props: {
-            posts: posts.slice(0, 6) // Limit to 6 posts
+            posts: posts.slice(0, 6)
         }
     };
 }
