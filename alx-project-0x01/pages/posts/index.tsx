5870c8a6780731119ff45055957482589c288d8e
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-11-14 14:58:56 +0300

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,10 +1,15 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage: React.FC = () => {
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
-            <p>This is the posts index page.</p>
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow container mx-auto p-4">
+                <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
+                <p>This is the posts index page.</p>
+            </main>
         </div>
-    )
-}
+    );
+};
 
 export default PostsPage;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,15 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Users Page</h1>
-            <p>This is the users index page.</p>
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow container mx-auto p-4">
+                <h1 className="text-3xl font-bold mb-4">Users Page</h1>
+                <p>This is the users index page.</p>
+            </main>
         </div>
-    )
-}
+    );
+};
 
 export default UsersPage;
