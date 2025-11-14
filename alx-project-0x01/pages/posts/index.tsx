f75332fa22a9671d76d55648fcf2f3f253e331a2
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-11-14 15:08:07 +0300

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,15 +1,50 @@
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-const PostsPage: React.FC = () => {
+interface PostsPageProps {
+    posts: PostProps[];
+}
+
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
+    console.log(posts);
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-grow container mx-auto p-4">
-                <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
-                <p>This is the posts index page.</p>
+            <main className="p-4 flex-grow bg-gray-50">
+                <div className="container mx-auto">
+                    <div className="flex justify-between items-center mb-6">
+                        <h1 className="text-2xl font-semibold">Post Content</h1>
+                        <button className="bg-blue-700 px-4 py-2 rounded-full text-white hover:bg-blue-800 transition">
+                            Add Post
+                        </button>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        {posts?.map((post: PostProps) => (
+                            <PostCard 
+                                key={post.id}
+                                title={post.title} 
+                                body={post.body} 
+                                userId={post.userId} 
+                                id={post.id} 
+                            />
+                        ))}
+                    </div>
+                </div>
             </main>
         </div>
     );
 };
 
-export default PostsPage;
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    
+    return {
+        props: {
+            posts: posts.slice(0, 6) // Limit to 6 posts for better display
+        }
+    };
+}
+
+export default Posts;
